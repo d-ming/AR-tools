82023018e8459e2bdcdedd9cee9e13c296ea5e44
d-ming <david.ming.za@gmail.com>
2016-08-14 12:55:32 +0200

    More tests for in_region
    Added more tests for in_region() to check for type as well as a few
    simple 3-D examples.

--- artools/test/in_region_test.py
+++ artools/test/in_region_test.py
@@ -4,10 +4,11 @@ import artools
 artools = reload(artools)
 
 import scipy as sp
+import pytest
 
 
 def test_in_1():
-    # 0-D b vector
+    # simple 2-D triangle case
     A = sp.array([[-1., 0],
                   [0, -1],
                   [1, 1]])
@@ -20,7 +21,7 @@ def test_in_1():
 
 
 def test_in_2():
-    # 0-D b vector
+    # simple 2-D triangle case
     A = sp.array([[-1., 0],
                   [0, -1],
                   [1, 1]])
@@ -28,6 +29,20 @@ def test_in_2():
     b = sp.array([0., 0, 1])
 
     xi = sp.array([0.25, 0.1])
+
+    assert artools.in_region(xi, A, b)
+
+
+def test_in_3():
+    # simple 3-D case
+    A = sp.array([[-1., 0, 0],
+                  [0, -1, 0],
+                  [0, 0, -1],
+                  [1, 1, 1]])
+
+    b = sp.array([0., 0, 0, 1])
+
+    xi = sp.array([0.25, 0.1, 0.25])
 
     assert artools.in_region(xi, A, b)
 
@@ -58,8 +73,22 @@ def test_out_2():
     assert (artools.in_region(xi, A, b) is False)
 
 
+def test_out_3():
+    # simple 3-D case
+    A = sp.array([[-1., 0, 0],
+                  [0, -1, 0],
+                  [0, 0, -1],
+                  [1, 1, 1]])
+
+    b = sp.array([0., 0, 0, 1])
+
+    xi = sp.array([0.7, 0.7, 0.7])
+
+    assert (artools.in_region(xi, A, b) is False)
+
+
 def test_on_1():
-    # on a hyperplane
+    # on a 2-D hyperplane
     A = sp.array([[-1., 0],
                   [0, -1],
                   [1, 1]])
@@ -126,3 +155,57 @@ def test_tol_out_1():
     xi = sp.array([0.0 - 2*tol, 0.0 + tol])
 
     assert (artools.in_region(xi, A, b, tol=tol) is False)
+
+
+def test_shape_1():
+    # b is a 2-D column vector
+    A = sp.array([[-1., 0],
+                  [0, -1],
+                  [1, 1]])
+
+    b = sp.array([[0., 0, 1]]).T
+
+    xi = sp.array([0.25, 0.1])
+
+    assert artools.in_region(xi, A, b)
+
+
+def test_shape_2():
+    # b is a 2-D row vector
+    A = sp.array([[-1., 0],
+                  [0, -1],
+                  [1, 1]])
+
+    b = sp.array([[0., 0, 1]])
+
+    xi = sp.array([0.25, 0.1])
+
+    assert artools.in_region(xi, A, b)
+
+
+def test_shape_3():
+    # A and b have incompatible shapes
+    A = sp.array([[-1., 0],
+                  [0, -1],
+                  [1, 1]])
+
+    b = sp.array([[0., 1]])
+
+    xi = sp.array([0.25, 0.1])
+
+    with pytest.raises(ValueError):
+        artools.in_region(xi, A, b)
+
+
+def test_shape_4():
+    # xi has incompatible shape
+    A = sp.array([[-1., 0],
+                  [0, -1],
+                  [1, 1]])
+
+    b = sp.array([[0., 0, 1]])
+
+    xi = sp.array([0.25, 0.1, 0.1])
+
+    with pytest.raises(ValueError):
+        artools.in_region(xi, A, b)

e2c4f6e4f1f34ce2988d6da660ea2b7e29a68e63
d-ming <david.ming.za@gmail.com>
2017-09-07 20:57:33 +0200

    Added more tests

--- artools/test/genStoichMat_test.py
+++ artools/test/genStoichMat_test.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 sys.path.append('../')
 import artools
@@ -9,6 +10,7 @@ class TestTuple:
 
     def test_1(self):
 
+        # reactions written in tuple format
         rxn_str = ("A -> B",)
 
         A = artools.genStoichMat(rxn_str)
@@ -20,18 +22,7 @@ class TestTuple:
 
 class TestNormal:
 
-    def test_1(self):
-
-        rxn_str = ["A -> B"]
-
-        A = artools.genStoichMat(rxn_str)
-        A_ref = sp.array([[-1.0],
-                          [1.0]])
-
-        assert (artools.sameRows(A.T, A_ref.T) is True)
-
-
-    def test_2(self):
+    def test_vdv3D(self):
 
         rxn_str = ["A -> B",
                    "B -> C",
@@ -42,6 +33,34 @@ class TestNormal:
                           [1, -1, 0],
                           [0, 1, 0],
                           [0, 0, 1]])
+
+        assert (artools.sameRows(A.T, A_ref.T) is True)
+
+
+    def test_BTX(self):
+        rxns = ['B + 0.5*E -> T',
+                'T + 0.5*E -> X',
+                '2*B -> D + H']
+
+        A_ref = sp.array([[-1.0, 0.0, -2.0],
+                          [-0.5, -0.5, 0.0],
+                          [1.0, -1.0, 0.0],
+                          [0.0, 1.0, 0.0],
+                          [0.0, 0.0, 1.0],
+                          [0.0, 0.0, 1.0]])
+
+        A = artools.genStoichMat(rxns)
+
+        assert (artools.sameRows(A, A_ref) is True)
+
+
+    def test_1(self):
+
+        rxn_str = ["A -> B"]
+
+        A = artools.genStoichMat(rxn_str)
+        A_ref = sp.array([[-1.0],
+                          [1.0]])
 
         assert (artools.sameRows(A.T, A_ref.T) is True)
 
@@ -61,3 +80,51 @@ class TestNormal:
                           [0, 0, 0.1]])
 
         assert (artools.sameRows(A.T, A_ref.T) is True)
+
+
+class TestRealistic:
+
+    def test_NH3_1(self):
+        rxn = ["N2 + 3*H2 -> 2*NH3"]
+
+        A_ref = sp.array([[-1.0, -3.0, 2.0]]).T
+        A = artools.genStoichMat(rxn)
+
+        assert (artools.sameRows(A, A_ref) is True)
+
+
+    def test_CH4_reform(self):
+        rxns = ['CH4 + H2O -> CO + 3*H2',
+                'CO + H2O -> CO2 + H2']
+
+        stoich_mat = sp.array([[-1.0, 0.0],
+                               [-1.0, -1.0],
+                               [1.0, -1.0],
+                               [3.0, 1.0],
+                               [0.0, 1.0]])
+        A = artools.genStoichMat(rxns)
+
+        assert (artools.sameRows(stoich_mat, A) is True)
+
+
+    def test_UCG_1(self):
+        rxns = ['C + O2 -> CO2',
+                'C + CO2 -> 2*CO',
+                'CO + 0.5*O2 -> CO2',
+                'C + H2O -> CO + H2',
+                'CO + H2O -> CO2 + H2',
+                'CO2 + H2 -> CO + H2O',
+                'C + 2*H2 -> CH4',
+                'CH4 + H2O -> CO + 3*H2',
+                'CO + 3*H2 -> CH4 + H2O']
+
+        A_ref = sp.array([[-1., -1, 0, -1, 0, 0, -1, 0, 0],
+                          [-1, 0, -0.5, 0, 0, 0, 0, 0, 0],
+                          [1, -1, 1, 0, 1, -1, 0, 0, 0],
+                          [0, 2, -1, 1, -1, 1, 0, 1, -1],
+                          [0, 0, 0, -1, -1, 1, 0, -1, 1],
+                          [0, 0, 0, 1, 1, -1, -2, 3, -3],
+                          [0, 0, 0, 0, 0, 0, 1, -1, 1]])
+        A = artools.genStoichMat(rxns)
+
+        assert (artools.sameRows(A, A_ref) is True)

13f3323df3f064b4486b7c80b92cac05d26d0ef9
d-ming <david.ming.za@gmail.com>
2017-09-23 22:14:20 +0200

    Clean up nullspace tests

--- artools/test/nullspace_test.py
+++ artools/test/nullspace_test.py
@@ -5,69 +5,79 @@ from artools import sameRows, nullspace, rank
 import scipy as sp
 
 
-def test_1():
-    # 0-D nullspace Identity matrix
-    I = sp.eye(5)
+class Test0D:
 
-    N = nullspace(I)
+    def test_1(self):
+        # 0-D nullspace Identity matrix
+        I = sp.eye(5)
 
-    assert (N.shape == (5, 0))
+        N = nullspace(I)
 
-
-def test_2():
-    # 1-D nullspace
-    A = sp.array([[1., 0, 0],
-                  [0, 1, 0]])
-
-    N = nullspace(A)
-    N_ref = sp.array([[0., 0, 1]]).T
-
-    assert (sameRows(N, N_ref) is True)
+        assert (N.shape == (5, 0))
 
 
-def test_3():
-    # 2-D nullspace
-    A = sp.array([[1., 0, 0]])
+class Test1D:
 
-    N = nullspace(A)
-    N_ref = sp.array([[0., 0],
-                      [1, 0],
-                      [0, 1]])
+    def test_1(self):
+        # 1-D nullspace
+        A = sp.array([[1., 0, 0],
+                      [0, 1, 0]])
 
-    assert (sameRows(N, N_ref) is True)
+        N = nullspace(A)
+        N_ref = sp.array([[0., 0, 1]]).T
 
-
-def test_4():
-    # 1-D subspace in 5-D
-    A = sp.array([[1., 0, 0, 0, 0],
-                  [0, 1, 0, 0, 0],
-                  [0, 0, 1, 0, 0],
-                  [0, 0, 0, 1, 0]])
-
-    N = nullspace(A)
-    N_ref = sp.array([[0., 0, 0, 0, 1]]).T
-
-    assert (sameRows(N, N_ref) is True)
+        assert (sameRows(N, N_ref) is True)
 
 
-def test_5():
-    # test a 3x2 matrix
-    A = sp.array([[1., 0],
-                  [0, 1],
-                  [0, 0]])
+    def test_2(self):
+        # 1-D subspace in 5-D
+        A = sp.array([[1., 0, 0, 0, 0],
+                      [0, 1, 0, 0, 0],
+                      [0, 0, 1, 0, 0],
+                      [0, 0, 0, 1, 0]])
 
-    N = nullspace(A)
+        N = nullspace(A)
+        N_ref = sp.array([[0., 0, 0, 0, 1]]).T
 
-    assert N.shape == (2, 0)
+        assert (sameRows(N, N_ref) is True)
 
 
-def test_6():
-    # dimension of the nullspace
-    A = sp.array([[1., 2., 3., 1.],
-                  [1., 1., 2., 1.],
-                  [1., 2., 3., 1.]])
+class Test2D:
 
-    N = nullspace(A)
-    dimension = rank(N)
+    def test_1(self):
+        # 2-D nullspace
+        A = sp.array([[1., 0, 0]])
 
-    assert dimension == 2
+        N = nullspace(A)
+        N_ref = sp.array([[0., 0],
+                          [1, 0],
+                          [0, 1]])
+
+        assert (sameRows(N, N_ref) is True)
+
+
+class TestShape:
+
+    def test_1(self):
+        # test a 3x2 matrix
+        A = sp.array([[1., 0],
+                      [0, 1],
+                      [0, 0]])
+
+        N = nullspace(A)
+
+        assert N.shape == (2, 0)
+
+
+class TestDimension:
+
+    def test_1(self):
+        # dimension of the nullspace
+        A = sp.array([[1., 2., 3., 1.],
+                      [1., 1., 2., 1.],
+                      [1., 2., 3., 1.]])
+
+        N = nullspace(A)
+        dimension = rank(N)
+
+        assert dimension == 2
